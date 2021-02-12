283dd58af0f241640e0b6b16fa98d7ee3c48d5b7
Michael Pingel <mdpingel@gmail.com>
2021-02-12 14:30:05 -0800

    add a fake currency to dropdown to test errors, add css to make errors red then remove fake currency

--- src/main.js
+++ src/main.js
@@ -1,21 +1,33 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
 import ExchangeRateService from './services/exchange-rate-service.js';
 import ExchangeRate from './js/exchange-rate.js';
-// import './css/styles.css';
 
 
 function getElements(response, usd, currency) {
+  const exchange = new ExchangeRate();
+  const capitalCurrency = currency.toUpperCase();
   if (response.result === "success") {
-    const exchange = new ExchangeRate();
-    const capitalCurrency = currency.toUpperCase();
-    const returnString = exchange.calculateCurrency(usd, currency, response.conversion_rates[capitalCurrency]);
-    $('.showExchangeValue').text(returnString);
+    if (capitalCurrency in response.conversion_rates) {
+      const returnString = exchange.calculateCurrency(usd, currency, response.conversion_rates[capitalCurrency]);
+      $('.showExchangeValue').show();
+      $('.showExchangeValue').text(returnString);
+      $('.showErrors').hide();
+    } else {
+      $('.showErrors').show();
+      $('.showErrors').text(`${capitalCurrency} is not a valid currency. Please enter a valid currency.`);
+      $('.showExchangeValue').hide();
+    }
   } else if (response.result === "error") {
-    $('.showErrors').text(`${response["error-type"]}`);
+    $('.showErrors').show();
+    $('.showErrors').text(`Error: ${response["error-type"]}`);
+    $('.showExchangeValue').hide();
   } else {
+    $('.showErrors').show();
     $('.showErrors').text("borked");
+    $('.showExchangeValue').hide();
   }
 }
 
