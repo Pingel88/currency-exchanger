874df57b860e2798e6e0ec3355b3b0638237f144
Michael Pingel <mdpingel@gmail.com>
2021-02-12 10:16:41 -0800

    add user interface to index.html

--- src/main.js
+++ src/main.js
@@ -1,32 +1,32 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import WeatherService from './weather-service.js';
+// import ExchangeRate from './services/exchangerate-service.js';
 // import './css/styles.css';
 
-function clearFields() {
-  $('#location').val("");
-  $('#showErrors').val("");
-  $('#showHumidity').val("");
-  $('#showTemp').val("");
-}
+// function clearFields() {
+//   $('#location').val("");
+//   $('#showErrors').val("");
+//   $('#showHumidity').val("");
+//   $('#showTemp').val("");
+// }
 
-function getElements(response) {
-  if (response.main) {
-    $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
-    $('.showTemp').text(`The temperature in Kelvin is ${response.main.temp} degrees.`);
-  } else {
-    $('.showErrors').text(`There was an error processing your request: ${response}`);
-  }
-}
+// function getElements(response) {
+//   if (response.main) {
+//     $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+//     $('.showTemp').text(`The temperature in Kelvin is ${response.main.temp} degrees.`);
+//   } else {
+//     $('.showErrors').text(`There was an error processing your request: ${response}`);
+//   }
+// }
 
-$(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    let city = $('#location').val();
-    clearFields();
-    (async function() {
-      const response = await WeatherService.getWeather(city);
-      getElements(response);
-    })();
-  });
-});
+// $(document).ready(function() {
+//   $('#weatherLocation').click(function() {
+//     let city = $('#location').val();
+//     clearFields();
+//     (async function() {
+//       const response = await WeatherService.getWeather(city);
+//       getElements(response);
+//     })();
+//   });
+// });
